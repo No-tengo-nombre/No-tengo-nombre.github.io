7391dee8fed67e9d00405229d8bcd0569b161f82
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2021-12-16 04:42:40 -0300

    Working language buttons

--- js/langSelect.js
+++ js/langSelect.js
@@ -1,19 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
     let zones = document.querySelectorAll("html[lang]");
     applyStrings(zones);
-
-    let lang = findLocaleMatch();
-    let container = document.querySelector(`html [lang*=${lang}]`);
-    console.log(container);
-    container.className = "lang-match";
 });
 
 function applyStrings(containers) {
     containers.forEach(container => {
-        let locale = container.getAttribute("lang");
+        let locale;
+        let hashVal = location.hash;
+
+        if (hashVal) {
+            locale = hashVal.substring(1, 3);
+            container.setAttribute("lang", locale);
+        }
+        else {
+            locale = container.getAttribute("lang");
+        }
+        console.log(locale);
         container.querySelectorAll("[data-key]").forEach(element => {
             let key = element.getAttribute("data-key");
-            let lang = locale.substr(0, 2);
+            let lang = locale.substring(0, 2);
             if (key) {
                 element.textContent = langdata.languages[lang].strings[key];
             }
@@ -21,19 +26,8 @@ function applyStrings(containers) {
     });
 }
 
-function findLocaleMatch() {
-    let keys = Object.keys(langdata.languages);
-    let locales = Intl.getCanonicalLocales(keys);
-    let lang = navigator.language;
-    let locale = Intl.getCanonicalLocales(lang);
-
-    let langMatch = document.documentElement.getAttribute("lang");
-    locales = locales.filter(l => locale == l);
-    langMatch = (locales.length > 0) ? locales[0] : langMatch;
-    return langMatch;
-}
-
 function setLanguage(lang) {
     document.documentElement.setAttribute("lang", lang);
+    location.hash = lang;
     location.reload();
 }
